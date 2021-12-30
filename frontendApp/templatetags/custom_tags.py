48e2abcf432780b7ff77a630ea83fdce494c1107
Naznin Nahar <naznin.nahar@accelx.net>
2021-12-31 01:03:15 +0600

    .env file added

--- frontendApp/templatetags/custom_tags.py
+++ frontendApp/templatetags/custom_tags.py
@@ -1,7 +1,15 @@
 from django import template
 from datetime import datetime, timedelta
 import pytz
+import time
+
+from datetime import datetime
+from django.utils import timezone
+
+
 register = template.Library()
+
+
 
 @register.filter(name='convert_only_dateYear')
 def convert_date(given_time):
@@ -12,10 +20,30 @@ def convert_date(given_time):
 
 @register.filter(name='convert_only_time')
 def convert_time(given_time):
-    d = str(given_time)
-    timestamp = datetime.fromtimestamp(int(d))
-    final_result = timestamp.strftime("%I:%M %p")
-    return final_result
+    # d = str(given_time)
+    # timestamp = datetime.fromtimestamp(int(d))
+    # final_result = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    # return final_result
+    
+    # unix_timestamp = int("1640911219")
+    # utc_time = time.gmtime(unix_timestamp)
+    # local_time = time.localtime(unix_timestamp)
+    # final_result = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+    # print(final_result)
+    # return final_result
+    
+    unix_timestamp = int("1640911219")
+    local_time = time.localtime(unix_timestamp)
+    print(time.strftime("%Y-%m-%d %H:%M:%S %p", local_time))
+    result = time.strftime("%Y-%m-%d %H:%M:%S %p", local_time)
+    return result
+
+@register.filter(name='unix_to_datetime')
+def unix_to_datetime(value):
+    a = datetime(value)
+    result = a.strftime("%H:%M:%S %Z")
+    return result
+    
 
 
 
--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jn4r$(q6$5qle5mzpgwx5dmf6@&8!al-l1pfw)8g9$%*8_0!n1'
+#SECRET_KEY = 'django-insecure-jn4r$(q6$5qle5mzpgwx5dmf6@&8!al-l1pfw)8g9$%*8_0!n1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = []
 
@@ -94,10 +96,6 @@ DATABASES = {
         'PORT': '5432',
     }
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 
 
